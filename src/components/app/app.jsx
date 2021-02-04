7ed48bc71f26de1cf2989906b73a6897978e5a11
TimurDov11 <65161688+TimurDov11@users.noreply.github.com>
2021-02-05 01:16:35 +0500

    Добавляет компоненты

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainScreen from '../main-screen/main-screen';
+
+const App = () => {
+  //const {errorsCount} = props;
+
+  return (
+    <MainScreen />
+  );
+}
+
+export default App;
