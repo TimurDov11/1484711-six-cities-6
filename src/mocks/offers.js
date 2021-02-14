74ec5897f1de7ab6b6218fd1f384a6bb1338636c
TimurDov11 <65161688+TimurDov11@users.noreply.github.com>
2021-02-15 00:38:53 +0500

    Создаёт моки для предложений

---
+++ src/mocks/offers.js
@@ -0,0 +1,83 @@
+export default [
+  {
+    id: `1`,
+    photos: [`img/room.jpg`, `img/apartment-01.jpg`, `img/apartment-02.jpg`, `img/apartment-03.jpg`, `img/studio-01.jpg`, `img/apartment-01.jpg`]
+    title: `Beautiful &amp; luxurious apartment at great location`,
+    description: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`, `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`],
+    isPremium: true,
+    type: `apartment`,
+    rating: `4.8`
+    bedroomsNumber: `3`,
+    adultsNumber: `4`,
+    price: `120`,
+    insideList: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`, `Coffee machine`, `Baby seat`, `Kitchen`, `Dishwasher`, `Cabel TV`, `Fridge`],
+    host: {
+      src: `img/avatar-angelina.jpg`,
+      name: `Angelina`,
+      isPro: true,
+      id: `1`
+    }
+    isFavorite: true,
+    previewPhoto: `img/room.jpg`
+  }, {
+    id: `2`,
+    photos: [`img/room.jpg`, `img/apartment-01.jpg`, `img/apartment-02.jpg`, `img/studio-01.jpg`, `img/apartment-01.jpg`]
+    title: `Beautiful &amp; luxurious apartment at great location`,
+    description: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`, `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`],
+    isPremium: true,
+    type: `room`,
+    rating: `4.0`
+    bedroomsNumber: `10`,
+    adultsNumber: `1`,
+    price: `320`,
+    insideList: [`Washing machine`, `Towels`, `Coffee machine`, `Baby seat`, `Kitchen`, `Cabel TV`],
+    host: {
+      src: `img/studio-01.jpg`,
+      name: `Linda`,
+      isPro: true,
+      id: `8`
+    }
+    isFavorite: false,
+    previewPhoto: `img/room.jpg`
+  }, {
+    id: `3`,
+    photos: [`img/room.jpg`, `img/apartment-02.jpg`, `img/apartment-03.jpg`, `img/studio-01.jpg`, `img/apartment-01.jpg`]
+    title: `Beautiful &amp; luxurious apartment at great location`,
+    description: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`, `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`],
+    isPremium: true,
+    type: `house`,
+    rating: `3.5`
+    bedroomsNumber: `7`,
+    adultsNumber: `2`,
+    price: `200`,
+    insideList: [`Wi-Fi`, `Washing machine`, `Heating`, `Coffee machine`, `Kitchen`, `Dishwasher`, `Cabel TV`, `Fridge`],
+    host: {
+      src: `img/avatar-angelina.jpg`,
+      name: `Tom`,
+      isPro: false,
+      id: `5`
+    }
+    isFavorite: false,
+    previewPhoto: `img/apartment-03.jpg`
+  }, {
+    id: `4`,
+    photos: [`img/apartment-01.jpg`, `img/apartment-02.jpg`, `img/apartment-03.jpg`, `img/apartment-01.jpg`]
+    title: `Beautiful &amp; luxurious apartment at great location`,
+    description: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`, `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`, `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`],
+    isPremium: false,
+    type: `hotel`,
+    rating: `2.1`
+    bedroomsNumber: `2`,
+    adultsNumber: `5`,
+    price: `100`,
+    insideList: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`, `Baby seat`, `Kitchen`, `Dishwasher`, `Cabel TV`, `Fridge`],
+    host: {
+      src: `img/avatar-angelina.jpg`,
+      name: `Max`,
+      isPro: true,
+      id: `2`
+    }
+    isFavorite: true,
+    previewPhoto: `img/apartment-01.jpg`
+  }
+];
