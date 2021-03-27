2dc6b1815913a1f2b8d3f476bca42f140d108d18
TimurDov11 <65161688+TimurDov11@users.noreply.github.com>
2021-03-27 17:58:41 +0500

    Дорабатывает компонент LoginScreen и реализует авторизацию пользователя в приложении по маршруту /login

--- src/components/login-screen/login-screen.jsx
+++ src/components/login-screen/login-screen.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Link} from 'react-router-dom';
-//  import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {login} from "../../store/api-actions";
 
-const LoginScreen = () => {
+const LoginScreen = ({city, onSubmit}) => {
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  //  const history = useHistory();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    onSubmit({
+      login: loginRef.current.value,
+      password: passwordRef.current.value,
+    });
+  };
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
@@ -32,14 +48,14 @@ const LoginScreen = () => {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" action="#" method="post" onSubmit={handleSubmit}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required="" />
+                <input ref={loginRef} className="login__input form__input" type="email" name="email" placeholder="Email" required="" />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required="" />
+                <input ref={passwordRef} className="login__input form__input" type="password" name="password" placeholder="Password" required="" />
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
@@ -47,7 +63,7 @@ const LoginScreen = () => {
           <section className="locations locations--login locations--current">
             <div className="locations__item">
               <a className="locations__item-link" href="#">
-                <span>Amsterdam</span>
+                <span>{city}</span>
               </a>
             </div>
           </section>
@@ -57,4 +73,20 @@ const LoginScreen = () => {
   );
 };
 
-export default LoginScreen;
+LoginScreen.propTypes = {
+  city: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  city: state.city,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  }
+});
+
+export {LoginScreen};
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
